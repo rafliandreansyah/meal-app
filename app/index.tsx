fda9b5d1e75e125d02a7999e36990aed045cee71
Rafli andreansyah <47078618+rafliandreansyah@users.noreply.github.com>
2025-08-13 23:35:45 +0700

    feat: add new library react navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet } from "react-native";
 import CategoryScreen from "./screens/CategoryScreen";
 
+const Stack = createNativeStackNavigator();
+
 export default function Index() {
-  return <CategoryScreen />;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Categories" component={CategoryScreen} />
+    </Stack.Navigator>
+  );
 }
 
 const styles = StyleSheet.create({
